21d6d5908cf4cdafb8dfd0b0ca0f753240d0d31b
Shekhar Rajepandhare <shekharr@tavisca.com>
2019-12-29 20:58:42 +0530

    added applicaition-dev.properties to configuration trasnformation

--- src/main/java/com/crystalpizaa/api/CrystalPizzaApplication.java
+++ src/main/java/com/crystalpizaa/api/CrystalPizzaApplication.java
@@ -1,11 +1,7 @@
 package com.crystalpizaa.api;
 
-import org.springframework.aop.scope.ScopedProxyUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 //@SpringBootApplication(scanBasePackages = {"com.crystalpizaa.api.service.*"})
